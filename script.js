a1b01bc0af2953f7530ee6528fbe8232a0be5df8
brian <muriithibrian283@gmail.com>
2024-01-25 14:37:49 +0300

    Got unstuck. Use conditional statement plus and operator

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 // users picks either rock papers or scissors and his input is stored in a variable
-const usersInput = prompt("Please choose either rock, paper scissor");
-
 // Provide options (array stored in a a variable)for the program to pick
-const gameOptions = ["rock", "paper", "scissor"];
 // The program will pick a random word after the user inputs their choice
-const randomPick = gameOptions[Math.floor(Math.random() * gameOptions.length)]
-
 // Compare the users input with the programs choice
-function winner() {
-    if (gameOptions === randomPick) {
-        return 'draw'
-    } else 'you win'
+//Rules paper beats rock, rock beats scissors, scissors beats paper
 
+const getUserChoice = prompt("Please choose either rock, paper scissor");
+const playerSelection = getUserChoice.toLowerCase()
+const gameOptions = ["rock", "paper", "scissor"];
+
+const getComputerChoice = gameOptions[Math.floor(Math.random() * gameOptions.length)]
+
+function score() {
+    if (playerSelection === "rock" && getComputerChoice === "paper") {
+        return "You lose! Paper beats rock "
+    } else if (playerSelection === "paper" && getComputerChoice === "scissor") {
+        return "You lose! Scissors beat paper"
+    } else if (playerSelection === "scissor" && getComputerChoice === "rock") {
+        return "You lose! Rock beats scissor"
+
+    } else if (playerSelection === getComputerChoice) {
+        return "Thats a draw! Play again?"
+    }
+    else {
+        return ("You won! " + playerSelection + " beats " + getComputerChoice)
+    }
 }
-console.log(winner());
-// Rules paper beats rock, rock beats scissors, scissors beats paper
+console.log(score());
